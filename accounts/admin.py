a6c778b2b108e66af3d29c921ef274bc1161b13e
rohan-193260 <kandekarrohan555@gmail.com>
2026-06-19 15:44:13 +0530

    GS-108: customize Django admin - add ward_number filter and search fields

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,12 +2,16 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
+
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
     model = User
-    list_display  = ['username', 'email', 'phone_number', 'ward_number', 'role', 'is_staff']
-    list_filter   = ['role', 'is_staff', 'is_active']
-    fieldsets     = UserAdmin.fieldsets + (
+    list_display = ['username', 'email', 'phone_number', 'ward_number', 'role', 'is_staff']
+    list_filter = ['role', 'ward_number', 'is_staff', 'is_active']
+    search_fields = ['username', 'email', 'phone_number', 'ward_number']
+    ordering = ['ward_number', 'username']
+
+    fieldsets = UserAdmin.fieldsets + (
         ('Rural Parameters', {'fields': ('phone_number', 'ward_number', 'role')}),
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
