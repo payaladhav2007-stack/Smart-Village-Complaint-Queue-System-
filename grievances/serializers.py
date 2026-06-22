82962d182d07992f03803a2b8ff700993de9e91f
shaikhmaaz207 <animeustad@gmail.com>
2026-06-22 11:35:20 +0530

    GS-204: build grievance retrieval and status listing API

--- grievances/serializers.py
+++ grievances/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
 from .models import Complaint
 
-class ComplaintSerializer(serializers.ModelSerializer):
 
+class ComplaintSerializer(serializers.ModelSerializer):
     class Meta:
         model = Complaint
         fields = [
--- grievances/urls.py
+++ grievances/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import SubmitComplaintView
+from .views import SubmitComplaintView, ListComplaintsView
 
 urlpatterns = [
     path('submit/', SubmitComplaintView.as_view(), name='submit-complaint'),
+    path('list/', ListComplaintsView.as_view(), name='list-complaints'),
 ]
-
