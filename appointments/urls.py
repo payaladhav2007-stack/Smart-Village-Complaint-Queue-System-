ec3d2a898364bf836c200aa84de067c1e227134e
shaikhmaaz207 <animeustad@gmail.com>
2026-06-25 11:35:28 +0530

    GS-304: build token status and active queue retrieval API

--- appointments/urls.py
+++ appointments/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BookAppointmentView
+from .views import BookAppointmentView, ListMyAppointmentsView
 
 urlpatterns = [
     path('book/', BookAppointmentView.as_view(), name='book-appointment'),
+    path('my-tokens/', ListMyAppointmentsView.as_view(), name='my-tokens'),
 ]
