63ea5bd8ce927db80c741c936b2de25fea33720c
payaladhav2007-stack <payaladhav2007@gmail.com>
2026-06-25 12:36:25 +0530

    GS-306: Construct citizen-facing queue visibility dashboard

--- appointments/urls.py
+++ appointments/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import certificate_request
+from .views import certificate_request, queue_dashboard
 
 urlpatterns = [
     path('request/', certificate_request, name='certificate_request'),
+    path('queue/', queue_dashboard, name='queue_dashboard'),
 ]
--- appointments/views.py
+++ appointments/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 def certificate_request(request):
     return render(request, 'appointments/certificate_request.html')
-  
+
+def queue_dashboard(request):
+    return render(request, 'appointments/queue_dashboard.html')  
