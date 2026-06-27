0414195be2540b5884b093187f1b7da74ce5b51e
payaladhav2007-stack <payaladhav2007@gmail.com>
2026-06-27 13:44:33 +0530

    GS-406: Construct interactive infrastructure management rows

--- staff/urls.py
+++ staff/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import workbench
+from .views import workbench, grievance_management
 
 urlpatterns = [
     path('workbench/', workbench, name='workbench'),
+    path('grievances/', grievance_management, name='grievance_management'),
 ]
